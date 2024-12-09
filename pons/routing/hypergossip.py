09786a0710cf24b70bf89b9da6fb27f97ba6606c
fjrefluxx <45354454+fjrefluxx@users.noreply.github.com>
2024-12-09 17:44:59 +0100

    Multiple fixes and adaptations for HG

--- pons/routing/hypergossip.py
+++ pons/routing/hypergossip.py
@@ -1,10 +1,10 @@
 from copy import copy, deepcopy
 import pons
-from pons import Router, PayloadMessage, Hello
-from pons.event_log import event_log
 from typing import override
 import random
 import hashlib
+from .router import Router
+from simpy.util import start_delayed
 
 """
 TODO 
@@ -14,12 +14,64 @@ optional TODOs:
 - adaptations for dynamic beacon/hello intervals
 - 
 """
+class Beacon(pons.Hello):
+    """HG Beacon with additional cluster information"""
+
+    def __init__(self, src: int, created: float, message_hash: int, cluster_hash: int):
+        super().__init__(src, created)
+        self.cluster_hash = cluster_hash
+        self.message_hash = message_hash
+
+    @override
+    def get_size(self) -> int:
+        # add cluster and message hash (each 16 byte integers) to message size
+        return super().get_size() + 32
+
+    @override
+    def __str__(self):
+        return "BEACON(%s, src=%d, dst=%d, size=%d)" % (
+            self.unique_id(),
+            self.src,
+            self.dst,
+            self.size,
+        )
 
 
-class Hypergossip(Router):
+class IDsMessage(Beacon):
+    """Extended beacon containing known message IDs of a node"""
+
+    def __init__(self, src: int, created: float, message_ids: list[str], message_hash: int, cluster_hash: int):
+        self.IDs: list[str] = deepcopy(message_ids)
+        super().__init__(src, created, message_hash, cluster_hash)
+
+    def get_IDs(self) -> list[str]:
+        return deepcopy(self.IDs)
+
+    @override
+    def get_size(self) -> int:
+        # add msg IDs
+        # TODO could probably be improved by not using strings but UUIDs as identifiers?
+        s: int = super().get_size()
+        for mid in self.IDs:
+            s += len(mid.encode('utf-8'))
+        return s
+
+    @override
+    def __str__(self):
+        return "IDsMessage(%s, src=%d, dst=%d, size=%d)" % (
+            self.unique_id(),
+            self.src,
+            self.dst,
+            self.size,
+        )
+
+
+
+
+class HypergossipRouter(Router):
 
     def __init__(self, scan_interval=2.0, capacity=0, apps=None):
-        super(Hypergossip, self).__init__(scan_interval, capacity, apps)
+        super(HypergossipRouter, self).__init__(scan_interval, capacity, apps)
         # self.store = []
         self.next_beacon_contains_IDs = False
 
@@ -43,9 +95,14 @@ class Hypergossip(Router):
         """
         if self._store_add(msg):
             self.broadcast_queue.insert(0, msg)
+            self.log("Generated message %s added to bc queue" % msg)
 
             if not self.broadcasting_from_queue:
-                self.__broadcast_from_queue()
+                self.env.process(self.__broadcast_from_queue())
+
+
+        else:
+            print()
 
 
     @override
@@ -76,6 +133,8 @@ class Hypergossip(Router):
         else:
             super().on_receive(msg, remote_node_id)
 
+    def __broadcast_delay(self):
+        return random.uniform(0.001, self.broadcast_delay)
 
     def forward(self, msg: pons.PayloadMessage) -> None:
         """
@@ -83,6 +142,9 @@ class Hypergossip(Router):
         Only for received messages! Messages from a node's application must use the add function
         for correct handling.
         """
+        a = random.random()
+        b = self._gossip_probability()
+        print(a, b, a <= b)
 
         if random.random() <= self._gossip_probability():
             self.log("Gossip: Message %s added to broadcast queue." % msg)
@@ -90,21 +152,26 @@ class Hypergossip(Router):
 
             # start broadcast if required
             if not self.broadcasting_from_queue:
-                self.__broadcast_from_queue()
+                self.env.process(self.__broadcast_from_queue(self.__broadcast_delay()))
         else:
             self.log("Message %s was not forwarded because of gossip drop." % msg)
 
 
-    def __broadcast_from_queue(self) -> None:
+    def __broadcast_from_queue(self, initial_delay: float = 0) -> None:
         """
         Broadcast process to send packets from the queue.
         """
-
         if len(self.broadcast_queue) == 0:
             self.broadcasting_from_queue = False
             return
 
-        self.broadcasting_from_queue = True
+        if initial_delay > 0:
+            self.broadcasting_from_queue = True
+            self.log("Delaying broadcast from queue")
+            yield self.env.timeout(initial_delay)
+        else:
+            self.broadcasting_from_queue = True
+
         self.log("Start broadcast from queue.")
 
         # simpy event process
@@ -116,7 +183,7 @@ class Hypergossip(Router):
                 if isinstance(msg, pons.PayloadMessage) and not msg.is_expired(self.netsim.env.now):
                     break
                 else:
-                    self.log("Drop message %s from broadcast queue." % msg)
+                    self.log("Drop message %s from broadcast queue. Expired=%s" % (msg, msg.is_expired(self.netsim.env.now)))
             else:
                 # no valid messages, terminate broadcast process
                 self.broadcasting_from_queue = False
@@ -158,10 +225,10 @@ class Hypergossip(Router):
 
         # start broadcast if required
         if not self.broadcasting_from_queue:
-            self.__broadcast_from_queue()
+            self.env.process(self.__broadcast_from_queue(self.__broadcast_delay()))
 
 
-    def __check_beacon_process(self, msg: pons.Beacon) -> None:
+    def __check_beacon_process(self, msg: Beacon) -> None:
         """
         Check if the next beacon should contain more detailed information on known messages, based on the
         known message hash of another node's received beacon.
@@ -169,7 +236,8 @@ class Hypergossip(Router):
         :param msg: beacon message
         """
         if msg.src not in self.peers:
-            if not msg.cluster_hash == self.__local_buffer_hash():
+            if not msg.message_hash == self.__local_buffer_hash():
+                self.log("Next beacon with message IDs")
                 self.next_beacon_contains_IDs = True
 
 
@@ -188,7 +256,7 @@ class Hypergossip(Router):
         # concatenate strings
         concatenated_string = ''.join(sorted_strings)
         # calculate 16 byte-hash
-        hash_int = int(hashlib.md5(concatenated_string.encode()).digest())
+        hash_int = int.from_bytes(hashlib.md5(concatenated_string.encode()).digest())
         return hash_int
 
 
@@ -201,7 +269,7 @@ class Hypergossip(Router):
         # convert to string for hashing
         concatenated_integers = ''.join(map(str, sorted_integers))
         # calculate 16 byte-hash
-        hash_int = int(hashlib.md5(concatenated_integers.encode()).digest())
+        hash_int = int.from_bytes(hashlib.md5(concatenated_integers.encode()).digest())
         return hash_int
 
 
@@ -222,7 +290,7 @@ class Hypergossip(Router):
         Duplicate payload message received.
         If scheduled, remove received duplicates from the broadcast queue to reduce load on the wireless medium.
         """
-        self.log("Duplicated payload message received: %s from %d" % (msg, remote_node_id))
+        #self.log("Duplicated payload message received: %s from %d" % (msg, remote_node_id))
 
         if msg in self.broadcast_queue:
             self.log("Remove duplicated message from broadcast queue: %s" % msg)
@@ -234,18 +302,18 @@ class Hypergossip(Router):
         """
         Overwrite scan of router to allow sending beacons instead of hello messages.
         """
-        super.last_peer_found = self.netsim.env.now
+        self.last_peer_found = self.netsim.env.now
 
         while True:
             # self.peers.clear()
             # I think clearing all peers is not a good idea! Should be adapted to allow for missed beacons and more
             # adaptive neighborhood discovery approaches
-            for k in self.neighbors.keys():
+            for k in list(self.neighbors.keys()):
                 # remove neighbor if have not seen within multiple consecutive intervals
                 if self.neighbors.get(k) + self.neighborhood_removal_time < self.env.now:
                     self.neighbors.pop(k)
                     # also remove from router's peer list
-                    self.peers.pop(k)
+                    self.peers.remove(k)
 
             # send beacons only while not already transmitting
             if not self.broadcasting_from_queue:
@@ -255,18 +323,20 @@ class Hypergossip(Router):
                     self.netsim.nodes[self.my_id].send(
                         self.netsim,
                         pons.BROADCAST_ADDR,
-                        pons.IDsMessage(
+                        IDsMessage(
                             self.my_id,
                             self.netsim.env.now,
                             self.__local_buffer_keys(),
+                            self.__local_buffer_hash(),
                             self.__neighborhood_hash()
                         ),
                     )
+                    self.next_beacon_contains_IDs = False
                 else:
                     self.netsim.nodes[self.my_id].send(
                         self.netsim,
                         pons.BROADCAST_ADDR,
-                        pons.Beacon(
+                        Beacon(
                             self.my_id,
                             self.netsim.env.now,
                             self.__local_buffer_hash(),
@@ -279,40 +349,9 @@ class Hypergossip(Router):
         """
         Add the peer and the time of the reception to the neighborhood list.
         """
-        self.log("[%s] scan received: %s from %d" % (self.my_id, msg, remote_node_id))
+        # self.log("[%s] scan received: %s from %d" % (self.my_id, msg, remote_node_id))
         self.neighbors[remote_node_id] = self.env.now
 
 
 
-class Beacon(Hello):
-    """HG Beacon with additional cluster information"""
 
-    def __init__(self, src: int, created: float, message_hash: int, cluster_hash: int):
-        super().__init__(src, created)
-        self.cluster_hash = cluster_hash
-        self.message_hash = message_hash
-
-    @override
-    def size(self) -> int:
-        # add cluster and message hash (each 16 byte integers) to message size
-        return super().size() + 32
-
-
-class IDsMessage(Beacon):
-    """Extended beacon containing known message IDs of a node"""
-
-    def __init__(self, src: int, created: float, message_ids: list[str], cluster_id: int):
-        super().__init__(src, created, cluster_id)
-        self.IDs: list[str] = deepcopy(message_ids)
-
-    def get_IDs(self) -> list[str]:
-        return deepcopy(self.IDs)
-
-    @override
-    def size(self) -> int:
-        # add msg IDs
-        # TODO could probably be improved by not using strings but UUIDs as identifiers?
-        s: int = super().size()
-        for id in self.IDs:
-            s += len(id.encode('utf-8'))
-        return s
